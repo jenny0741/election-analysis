bc030b8f8ce4c6f64654ce81be5c46f0c9db3cad
Your Name <you@example.com>
2022-08-26 22:29:20 -0700

    Adding the election audit code

---
+++ Python_practice.py
@@ -0,0 +1,32 @@
+# print("Hello World")
+
+# my_votes = int(input("How many votes did you get in the election?"))
+
+# total_votes = int(input("What is the total votes in the election?"))
+
+# percentage_votes = (my_votes / total_votes) * 100
+
+# print(f"I received {my_votes / total_votes * 100}% of the total votes")
+
+# candidate_votes = int(input("how many votes did the candidate get in the election?"))
+
+# total_votes = int(input("what is the total number of votes in the election?"))
+
+# message_to_candidate = {
+
+#     f"you received {candidate_votes} number of votes"
+
+#     f"the total number of votes in the election is {total_votes}"
+
+#     f"you received {candidate_votes / total_votes * 100:.2f}% of the total votes"
+# }
+
+# print(message_to_candidate)
+
+# counties_dict = {
+#     "Araphoe": 422829,
+#     "Denver": 463353,
+#     "Jefferson": 432438
+# }
+
+# print(f"Araphoe county has {counties_dict['Araphoe']} registered voters")
